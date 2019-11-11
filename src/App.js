f4ea285688bef7f517197afcb5aee8a49836c4ab
Kaylah Malillos <malillosk@gmail.com>
2019-11-10 23:00:13 -0700

    added starter files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <p>
+      hello world!
+    </p>
+    );
+  }
+};
+
+// function App() {
+//   return (
+//     <div className="App">
+//             <p>
+//       hello world!
+//     </p>
+//     </div>
+//   );
+// }
+
+export default App;
